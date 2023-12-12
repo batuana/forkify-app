3305cf1d53c3d4461136b97be22702b2fd5445bb
batuana <batuhan.ozcan.dev@gmail.com>
2023-12-12 13:42:43 +0300

    API key change

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+// export const KEY = '<YOUR_KEY>';
+export const KEY = '35f81b30-510b-4339-957e-0838ec0130b4';
 export const MODAL_CLOSE_SEC = 2.5;
